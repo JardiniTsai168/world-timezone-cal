d31951ff360d4fc2321eefffc9b3001ee5afc877
JardiniTsai168 <bongzhu@gmail.com>
2026-05-11 14:53:10 +0800

    Phase 6.1: Configure iOS/Android for release
    - iOS Bundle ID: lihi.io.worldtimezonecal
    - Android Package: lihi.io.worldtimezonecal
    - App Name: World Timezone Calculator
    - Version: 1.0.0+1
    - Successfully built iOS (.app) and Android (.apk)
    - CocoaPods integrated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.world_timezone_cal"
+    namespace = "lihi.io.worldtimezonecal"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.world_timezone_cal"
+        applicationId = "lihi.io.worldtimezonecal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
